0a2a84762e369b8c0847ce8a2d47d0a1ea202abb
sachin saxena <sachininvertis744@gmail.com>
2026-03-11 20:42:18 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-X = [[30], [40], [50], [70]]
-Y = [0, 0, 1, 1]
-
-model = LogisticRegression()
-model.fit(X, Y)
-
-print(model.predict([[20]]))
